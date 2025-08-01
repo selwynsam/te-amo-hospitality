5c6ffd778a19a044afb86f95df6d84a2e2f984a4
selwynsam <selwynsam13@gmail.com>
2025-08-02 00:13:11 +0530

    Update Next.js configuration: change output directory to `dist`, enable unoptimized images, and add comments for potential basePath configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   // This enables static export
   output: "export",
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+
+  // basePath: process.env.NODE_ENV === "production" ? "/te-amo-hospitality" : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
