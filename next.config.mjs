7c123792482beb19f665a2bab62bc1b4d133864b
selwynsam <selwynsam13@gmail.com>
2025-08-02 00:59:54 +0530

    Update Next.js configuration: ensure basePath is correctly set for production environment.

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   // Optional: Change the output directory `out` -> `dist`
   distDir: "dist",
 
-  // basePath: process.env.NODE_ENV === "production" ? "/te-amo-hospitality" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/te-amo-hospitality" : "",
 
   images: {
     unoptimized: true,
